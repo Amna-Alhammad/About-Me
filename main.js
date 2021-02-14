ab7a04351b9417650e2677da9cc9918fcb3394c3
amna-alhammad <amnaq_1994@yahoo.com>
2021-02-14 13:20:53 +0200

    abc

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ let yourName = prompt('What is your name ?');
 console.log('don’t delete ' + yourName);
 alert( 'Welcom '+yourName + ' in my site :)' );
 
-let myName = prompt ('Please answer yes or no ......My name is Amna :)');
+let myName = prompt ('Please answer yes \'y or no \'n......My name is Amna :)');
  switch (myName.toLowerCase()) {
    case 'yes':
+   case 'y':
 console.log('don’t delete ' +myName);
      alert ('You are good, my name is Amna');
     
      break;
      case 'no':
+     case 'n':
       console.log('don’t delete ' + myName);
        alert ('NoOoOOo ,,,, my name is Amna');
        break;
@@ -19,14 +21,16 @@ console.log('don’t delete ' +myName);
      break;
  }
 
-let myAge = prompt ('Please answer yes or no ......I am 22 years old??)');
+let myAge = prompt ('Please answer yes \'y or no \'n ......I am 22 years old??)');
 switch (myAge.toLowerCase()) {
   case 'yes':
+  case 'y':
 console.log('don’t delete ' + myAge);
     alert ('Oh NoOo ,,,,,I am 26 years old ');
    
     break;
     case 'no':
+    case 'n':
      console.log('don’t delete ' + myAge);
       alert ('You are good, I am 26 years old');
       break;
@@ -35,14 +39,16 @@ console.log('don’t delete ' + myAge);
     break;
 }
 
-let myMajor = prompt ('Please answer yes or no ...... I studied computer science??)');
+let myMajor = prompt ('Please answer yes \'y or no \'n ...... I studied computer science??)');
 switch (myMajor.toUpperCase()) {
   case 'YES':
+    case 'Y':
 console.log('don’t delete ' + myMajor);
     alert ('YES ..... I am studied computer science');
    
     break;
     case 'NO':
+      case 'N':
      console.log('don’t delete ' + myMajor);
       alert ('NOOO ..... I am studied computer science');
       break;
@@ -51,31 +57,35 @@ console.log('don’t delete ' + myMajor);
     break;
 }
 
-let myDrink = prompt ('Please answer yes or no ...... My favorite drink is tea??)');
+let myDrink = prompt ('Please answer yes  \'y or no \'n ...... My favorite drink is tea??)');
 switch (myDrink.toUpperCase()) {
   case 'YES':
+ case 'Y':
 console.log('don’t delete ' + myDrink);
     alert ('NoOOoOo ..... My favorite drink is coffee');
    
     break;
     case 'NO':
-     console.log('don’t delete ' + myDrink);
-      alert ('Yesssss ..... My favorite drink is coffeee');
-      break;
+    case 'N':
+    console.log('don’t delete ' + myDrink);
+    alert ('Yesssss ..... My favorite drink is coffeee');
+    break;
   default:
     alert(myDrink);
     break;
 }
 
 
-let myGol = prompt ('Please answer yes or no ...... I strive to be a teacher ?)');
+let myGol = prompt ('Please answer yes \'y or no \'n...... I strive to be a teacher ?)');
 switch (myGol.toUpperCase()) {
   case 'YES':
+    case 'Y':
 console.log('don’t delete ' + myGol);
     alert ('NoOOoOo ..... I strive to be the best programmer');
    
     break;
     case 'NO':
+      case 'N':
      console.log('don’t delete ' + myGol);
       alert ('You are good...... I strive to be the best programmer');
       break;
